beb626433e65810db929895255bf95ca5f9f0768
Altynay18 <altynai.kzh17@gmail.com>
2022-07-20 17:02:45 +0600

    styles updated

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,6 +1,6 @@
 import styles from './Profile.module.scss';
 import UserCard from '../../components/UserCard';
-
+import {Tabs, TabList, TabPanels, Tab, TabPanel} from '@chakra-ui/react'
 import {Outlet} from 'react-router';
 
 const Profile = () => {
@@ -10,9 +10,30 @@ const Profile = () => {
       <UserCard />
       <div className={styles.infoSection}>
         <div className={styles.navs}>
-            
+          <Tabs>
+            <TabList>
+              <Tab fontWeight={"600"} backgroundColor={"#fff"} fontSize={'1rem'} color={'#564D80'}>Мои посты</Tab>
+              <Tab fontWeight={"600"} marginLeft={"3rem"} backgroundColor={"#fff"} fontSize={'1rem'} color={'#564D80'}>
+                Написать пост
+              </Tab>
+              <Tab fontWeight={"600"} marginLeft={"3rem"} backgroundColor={"#fff"} fontSize={'1rem'} color={'#564D80'}>
+                Мои проекты
+              </Tab>
+            </TabList>
+
+            <TabPanels>
+              <TabPanel
+              >
+              </TabPanel>
+              <TabPanel>
+              </TabPanel>
+              <TabPanel >
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
         </div>
         <Outlet />
+
       </div>
     </div>
   );
